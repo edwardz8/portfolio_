d47d40df2f876632b0e8bb0c8252f8296627a355
edwardz8 <tolklorist@gmail.com>
2019-12-06 17:07:53 -0600

    retry form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -169,13 +169,7 @@
           </div>
 
           <div class="text-lg sm:text-lg mb-16">
-            <form
-              class="mb-12"
-              name="contact"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
+            <form class="mb-12" name="contact" method="post" netlify action="/">
               <div class="flex flex-wrap mb-6 -mx-4">
                 <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
                   <label class="block mb-2 text-copy-primary" for="name">Name</label>
